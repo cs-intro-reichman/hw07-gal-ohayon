d72899bb8cf98c77f8c7bfcad903e984beb396fb
gal-ohayon <galohayon2222@gmail.com>
2025-12-28 15:04:23 +0200

    complite assainment

--- Palindrome.java
+++ Palindrome.java
@@ -1,13 +1,24 @@
 /** Checks if a given string is a palindrome. */
 public class Palindrome {
 
-	public static void main(String[]args) {
-    	System.out.println(isPalindrome(args[0]));
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println(isPalindrome(args[0]));
+        }
     }
-	
-	/** Checks if the given string is a palindrome. */
-	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+
+    public static boolean isPalindrome(String s) {
+        // תנאי עצירה: מחרוזת ריקה או באורך 1 היא פלינדרום
+        if (s == null || s.length() <= 1) {
+            return true;
+        }
+
+        // בדיקה אם האות הראשונה והאחרונה שוות
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+
+        // קריאה רקורסיבית על המחרוזת ללא האות הראשונה והאחרונה
+        return isPalindrome(s.substring(1, s.length() - 1));
     }
 }
